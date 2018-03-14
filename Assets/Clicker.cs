76b64e913c8b6099128f333b4d66146c880c9418
aaronatSAE <37372591+aaronatSAE@users.noreply.github.com>
2018-03-15 10:50:03 +1100

    Reverted to previous version.

--- Assets/Clicker.cs
+++ Assets/Clicker.cs
@@ -10,12 +10,16 @@ public class Clicker : MonoBehaviour
 	public Image image;
 	public float currentImageColorAlpha = 0.0f;
 
+<<<<<<< HEAD
 	void start()
 	{
 		image = GetComponent<Image> ();
 		currentImageColorAlpha = image.color.a;
 	}
 	void Update()
+=======
+    void Update()
+>>>>>>> parent of c50dc00... Change to sphere
     {
         if (Input.GetMouseButton(0))
         {
